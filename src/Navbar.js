5a907d2b2f71661382d66583355ddc311d597628
Mohammad <mohammad1f2f@gmail.com>
2022-01-30 22:20:19 +0330

    test

---
+++ src/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar() {
+  return ( 
+    <>  
+      <nav className="navbar">
+        <div>Block list</div>
+      </nav>
+    </>);
+
+}
+
+export default Navbar;
